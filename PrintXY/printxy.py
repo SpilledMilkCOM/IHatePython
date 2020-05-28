43b7400067fe3ff0c98f21926416aa05a1ef8b8b
Parker Smart <psmart@spilledmilk.com>
2020-05-28 07:38:57 -0600

    Adjust format to include leading zeros

--- PrintXY/printxy.py
+++ PrintXY/printxy.py
@@ -1,7 +1,7 @@
 import sys
 
-def printXY(n: int):
-    """Prints a string comprised of all combinations of X and Y up to width N.
+def print_xy(n: int):
+    """Prints a string comprised of all combinations of X and Y of length N.
     
     Args:
         n (int): The width of the string
@@ -10,8 +10,10 @@ def printXY(n: int):
     if n < 0:
         raise ValueError("ERROR: n must be greater than or equal to 0")
 
+    format_spec = f"0{n}b"          # Need leading zeros to include all combinations
+
     for i in range(2 ** n):
-        print(f"{i:b}".replace("0", "X").replace("1", "Y"))
+        print(format(i, format_spec).replace("0", "X").replace("1", "Y"))
 
 if __name__ == "__main__":
-    printXY(int(sys.argv[1]))
+    print_xy(int(sys.argv[1]))
